c173b4752e303b953241f77dbf1a226de6775a68
Abhishek <118333490+abhishek8123@users.noreply.github.com>
2023-07-31 21:18:31 +0530

    Update LogInnApplication.java

--- src/main/java/com/login/pkg/LogInnApplication.java
+++ src/main/java/com/login/pkg/LogInnApplication.java
@@ -2,6 +2,7 @@ package com.login.pkg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class LogInnApplication extends SpringBootServletInitializer {
